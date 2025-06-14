079deecafbfc524a5c73c626f4cc5771af2a7d3e
siddhant agrawal <siddhant-agrawal01>
2025-06-14 11:44:25 +0530

    17

--- server.js
+++ server.js
@@ -16,6 +16,7 @@
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(5000, () => console.log(`Server running on port 5000`));
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
